8a427564d94963e64731720eddfac7f1a5bf205a
Ankit bhavarthe <127740913+Ankit-2563@users.noreply.github.com>
2026-04-17 03:42:18 +0530

    ggs2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       name: "crux-ngrok",
       script: "ngrok",
-      args: "http 4001 --domain=unperishing-nonseasonably-yesenia.ngrok-free.dev",
+      args: "http 4001 --url=unperishing-nonseasonably-yesenia.ngrok-free.dev --authtoken=38zwnzS2SsBDTZJbVwkF6ydGkkJ_2poATXvnEXzmV4BtF5vTL",
       restart_delay: 5000,
     },
   ],
